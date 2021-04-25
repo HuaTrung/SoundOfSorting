5d48eb92415e2d1c7d4259a3066d54d159f3663d
Gnurt <huavitrung@gmail.com>
2021-04-25 21:52:16 +0700

    add more

---
+++ build/precache-manifest.e225af1c1e740d127669eafcb7c83f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822461b40ff203b8255ab877e4d6df75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb54eade85ff73deb391",
+    "url": "/static/css/main.88afd445.chunk.css"
+  },
+  {
+    "revision": "296aa1bc3c03f7c7b2d4",
+    "url": "/static/js/2.8dd87892.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8dd87892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb54eade85ff73deb391",
+    "url": "/static/js/main.acf00321.chunk.js"
+  },
+  {
+    "revision": "cbd2bf1dec8f01387e1e",
+    "url": "/static/js/runtime-main.6b5c2e67.js"
+  }
+]);
