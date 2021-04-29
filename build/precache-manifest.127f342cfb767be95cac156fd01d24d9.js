270a21c172b1753ba27eccf1d398a8a7b13cb030
Gnurt <huavitrung@gmail.com>
2021-04-29 23:07:08 +0700

    fix layout

---
+++ build/precache-manifest.127f342cfb767be95cac156fd01d24d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4014e25da5e5f95cb84342f2461293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3e2c9473154d097310",
+    "url": "/static/css/main.3539fffd.chunk.css"
+  },
+  {
+    "revision": "abfca7bc55485e375aa2",
+    "url": "/static/js/2.150bbe1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.150bbe1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3e2c9473154d097310",
+    "url": "/static/js/main.06089c63.chunk.js"
+  },
+  {
+    "revision": "cbd2bf1dec8f01387e1e",
+    "url": "/static/js/runtime-main.6b5c2e67.js"
+  }
+]);
